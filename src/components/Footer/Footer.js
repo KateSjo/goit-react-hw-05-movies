66d191a4da1bc0dc47cda62d0f1739b1c9351a4d
Jekaterina Sjomkina <olympik2010@inbox.lv>
2021-08-14 21:14:05 +0300

    fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import s from './Footer.module.css';
 import Container from "../Container";
-import Batman from "../../images/batman.png";
+import Batman from "../../images/harli.png";
 
 const Footer = () => {
     const today = new Date(),
@@ -8,7 +8,7 @@ const Footer = () => {
     return (
         <footer className={s.footer}>
             <Container>
-                <p className={s.copyright}>&#169; {year} | All Rights Reserved | Developed by <a className={s.link} href='https://github.com/VladymyrRzhanov/goit-react-hw-05-movies' target="_blank" rel="noreferrer"><img className={s.profile} src={Batman} alt='batman' /><span className={s.name}>Vladymyr Rzhanov</span></a></p>
+                <p className={s.copyright}>&#169; {year} | All Rights Reserved | Developed by <a className={s.link} href='https://github.com/KateSjo/goit-react-hw-05-movies' target="_blank" rel="noreferrer"><img className={s.profile} src={Batman} alt='harli' /><span className={s.name}>Jekaterina Sjomkina</span></a></p>
             </Container>
         </footer>
     )
